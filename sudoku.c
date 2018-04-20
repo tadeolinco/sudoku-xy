b09abd6697a640a4c872dc47a6b47fbf7fde4e53
Sam Bautista <tadeolinco@gmail.com>
2018-04-20 22:47:07 +0800

    Working with all 0s

--- sudoku.c
+++ sudoku.c
@@ -41,29 +41,32 @@ void print_stack(stack_node *ptr)
     }
     printf("\n");
   }
+  else
+  {
+    printf("\tStack empty!\n");
+  }
 }
 
 void validate(int **puzzle, int row, int col, int size, int **impossible)
 {
   int i, j;
 
-  int rowIndex;
   for (i = 0; i < size * size; ++i)
   {
-    // 0 is flag for possible
-    // 1 is flag for seen in puzzle
-    // rowIndex = puzzle[row][i];
-    // printf("row index: %d\n", rowIndex);
+    // Array of flags: 0 is flag for possible; 1 is flag for seen in puzzle
+    // Flags numbers present in its row
     if (puzzle[row][i] != 0)
     {
       (*impossible)[puzzle[row][i] - 1] = 1;
     }
+    // Flags numbers present in its column
     if (puzzle[i][col] != 0)
     {
       (*impossible)[puzzle[i][col] - 1] = 1;
     }
   }
 
+  // Flags numbers present within it's subsquare
   for (i = size * (row / size); i < size * (row / size) + size; ++i)
   {
     for (j = size * (col / size); j < size * (col / size) + size; ++j)
@@ -76,6 +79,27 @@ void validate(int **puzzle, int row, int col, int size, int **impossible)
   }
 }
 
+void pop(stack_list *ptr)
+{
+  stack_node *temp = ptr->stack;
+  ptr->stack = ptr->stack->next;
+  free(temp);
+}
+
+void write_solution(FILE *file_write, int solution, int **puzzle, int size)
+{
+  int i, j;
+  fprintf(file_write, "%d\n", solution);
+  for (i = 0; i < size * size; ++i)
+  {
+    for (j = 0; j < size * size; ++j)
+    {
+      fprintf(file_write, "%d ", puzzle[i][j]);
+    }
+    fprintf(file_write, "\n");
+  }
+}
+
 int main()
 {
   int i, j, k;
@@ -84,28 +108,28 @@ int main()
   stack_list *node, *temp = NULL;
   stack_list *root = NULL;
   stack_node *ptr;
-  int *impossible = NULL;
-  int **puzzle = NULL;
 
-  FILE *fp = NULL;
-  fp = fopen("graph.in", "r");
-  fscanf(fp, "%d", &num_of_puzzles);
+  FILE *file_read = NULL;
+  FILE *file_write = NULL;
 
-  printf("Num of puzzles: %d\n", num_of_puzzles);
+  file_read = fopen("input.txt", "r");
+  file_write = fopen("output.txt", "w");
+  fscanf(file_read, "%d", &num_of_puzzles);
+
   for (k = 0; k < num_of_puzzles; ++k)
   {
-    fscanf(fp, "%d", &size);
+    fscanf(file_read, "%d", &size);
 
-    puzzle = (int **)malloc(sizeof(int *) * size * size);
-    impossible = (int *)malloc(sizeof(int) * size * size);
+    int **puzzle = (int **)malloc(sizeof(int *) * size * size);
+    int *impossible = (int *)malloc(sizeof(int) * size * size);
+
     // create 2D puzzle
     for (i = 0; i < size * size; ++i)
     {
       puzzle[i] = (int *)malloc(sizeof(int) * size * size);
       for (j = 0; j < size * size; ++j)
       {
-        fscanf(fp, "%d", &puzzle[i][j]);
-        // printf("puzzle[i][j]: %d\n", puzzle[i][j]);
+        fscanf(file_read, "%d", &puzzle[i][j]);
 
         // if cell is 0, initialize stack for it
         if (puzzle[i][j] == 0)
@@ -119,9 +143,7 @@ int main()
 
           // attach node to last node in list of stack nodes
           if (root == NULL)
-          {
             root = node;
-          }
           else
           {
             temp = root;
@@ -143,10 +165,6 @@ int main()
         impossible[i] = 0;
       validate(puzzle, temp->row, temp->col, size, &impossible);
 
-      // for (i = 0; i < size * size; ++i)
-      //   printf("%d ", impossible[i]);
-      // printf("\n");
-
       // populates stack
       for (i = size * size - 1; i >= 0; --i)
       {
@@ -154,57 +172,67 @@ int main()
         {
           ptr = (stack_node *)malloc(sizeof(stack_node));
           ptr->x = i + 1;
-          if (temp->stack == NULL)
-          {
-            temp->stack = ptr;
-          }
-          else
-          {
-            ptr->next = temp->stack;
-            temp->stack = ptr;
-          }
+          ptr->next = temp->stack;
+          temp->stack = ptr;
         }
       }
-      printf("row: %d, col: %d\n", temp->row, temp->col);
 
-      // inserts value of the current nodes TOS into the puzzle
-      // this is to correctly validate the next node
-      if (temp->stack)
+      // if the current node didnt make a stack
+      // backtrack until you find a stack with more than 2 nodes
+      while (temp->stack == NULL)
       {
+        temp = temp->prev;
+        puzzle[temp->row][temp->col] = 0; // reset value of puzzle
+        pop(temp);
+      }
+
+      int solutions = 0;
+      // this means it reached the end of the puzzle
+      // and found a solution
+      if (temp->next == NULL)
+      {
+        printf("SOLUTION FOUND\n");
+        solutions++;
         puzzle[temp->row][temp->col] = temp->stack->x;
+        printf("Row: %d, Col: %d\n", temp->row, temp->col);
+        print_stack(temp->stack);
         print_puzzle(puzzle, size);
-      }
-      else
-      {
-        printf("going back\n");
-        while (temp->stack == NULL)
+        write_solution(file_write, solutions, puzzle, size);
+        // remove the the stack of the last node
+        puzzle[temp->row][temp->col] = 0;
+        pop(temp);
+
+        // keep removing stacks until it finds a stack that has 2 or more nodes
+        do
         {
-
-          // printf("setting temp back\n");
           temp = temp->prev;
-          // do pop
-          // printf("make a pointer to the current temp and it's x is %d\n", temp->stack->x);
-          stack_node *ptr = temp->stack;
-
-          // printf("repoint TOS to the next node\n");
-          temp->stack = temp->stack->next;
-
-          // puzzle[temp->row][temp->col] = temp->stack->x;
-          free(ptr);
-
-          printf("row: %d, col: %d\n", temp->row, temp->col);
-        }
+          // there's a possibility that it will reach the beginning
+          if (temp == NULL)
+            break;
+          puzzle[temp->row][temp->col] = 0;
+          pop(temp);
+        } while (temp->stack == NULL);
+        // has reached the beginning, the puzzle has been solved
+        if (temp == NULL)
+          break;
       }
+      // set the current cell of to the TOS for validation
+      puzzle[temp->row][temp->col] = temp->stack->x;
+
+      printf("Row: %d, Col: %d\n", temp->row, temp->col);
+      print_stack(temp->stack);
+      print_puzzle(puzzle, size);
+      // get next node to validate
       temp = temp->next;
     }
-
-    // for (i = 0; i < size * size; ++i)
-    // {
-    //   free(puzzle[i]);
-    // }
-    // free(puzzle);
-    // free(impossible);
+    for (i = 0; i < size * size; ++i)
+    {
+      free(puzzle[i]);
+    }
+    free(puzzle);
+    free(impossible);
   }
 
-  fclose(fp);
+  fclose(file_read);
+  fclose(file_write);
 }
